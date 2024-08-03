8d77734c966fb8badec70daf5ae776fc5df959fa
Marco Wettstein <marco.wettstein@pm.me>
2024-08-03 21:29:13 +0200

    started implementation and added replicache package

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,42 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { dev } from '$app/environment';
+	import { Replicache } from 'replicache';
+
+	type Todo = {
+		id: number;
+		name: string;
+		deleted: boolean;
+		createdAt: string;
+		updatedAt: string;
+	};
+
+	let todos: Todo[] = $state([]);
+	let form_state = $state({ name: '' });
+
+	let name = dev ? 'dev:todos:userId' : 'todos:userId';
+
+	let replicache = new Replicache({
+		name,
+		licenseKey: import.meta.env.VITE_REPLICACHE_LICENSE,
+		mutators: {}
+	});
+
+	function onsubmit(e: Event) {
+		e.preventDefault();
+	}
+</script>
+
+<form method="POST" {onsubmit}>
+	<label for="name">Todo:</label>
+	<input type="text" id="name" name="name" bind:value={form_state.name} />
+	<button type="submit">+ Add</button>
+</form>
+
+<ul>
+	{#each todos as todo}
+		<li>
+			<span>{todo.name}</span>
+			<button>✔</button>
+		</li>
+	{/each}
+</ul>
